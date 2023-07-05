36afd7620abac5ac7b16989a14b88e7ae1f3b501
Hamish Beath <33066830+hamishbeath@users.noreply.github.com>
2023-07-05 19:04:10 +0100

    System file updates

---
+++ src/clover_gui/details/system.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+
+import ttkbootstrap as ttk
+
+from ttkbootstrap.constants import *
+from ttkbootstrap.scrolled import *
+
+
+__all__ = ("FinanceFrame",)
+
+
+class SystemFrame(ttk.Frame):
+    """
+    Represents the System frame.
+
+    Contains System inputs for the system.
+
+    TODO: Update attributes.
+
+    """
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(4, weight=1)
+        self.rowconfigure(5, weight=1)
+        self.rowconfigure(6, weight=1)
+        self.rowconfigure(7, weight=1)
+        self.rowconfigure(8, weight=1)
+        self.rowconfigure(9, weight=1)
+        self.rowconfigure(10, weight=1)
+        self.rowconfigure(11, weight=1)
+        self.rowconfigure(12, weight=1)
+        self.rowconfigure(13, weight=1)
+        self.rowconfigure(14, weight=1)
+        self.rowconfigure(15, weight=1)
+        self.rowconfigure(16, weight=1)
+        self.rowconfigure(17, weight=1)
+        self.rowconfigure(18, weight=1)
+        self.rowconfigure(19, weight=1)
+        self.rowconfigure(20, weight=1)
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1)
+        self.columnconfigure(4, weight=1)
+
+        """
+        ac_transmission_efficiency: 0.95 # Efficiency of AC distribution network
+        dc_transmission_efficiency: 0.95 # Efficiency of DC distribution network
+        battery: default_battery
+        # clean_water_tank: cold_water_tank
+        conversion:
+        dc_to_ac: 0.95 # Conversion efficiency (0.0-1.0)
+        dc_to_dc: 0.95 # Conversion efficiency (0.0-1.0)
+        ac_to_dc: 0.8 # Conversion efficiency (0.0-1.0)
+        ac_to_ac: 0.98 # Conversion efficiency (0.0-1.0)
+        diesel_generator: default_diesel
+        # heat_exchanger: default_heat_exchanger
+        # hot_water_tank: hot_water_tank
+        pv_panel: default_pv
+        # pvt_panel: default_pvt
+        
+        """
+
+        # AC transmission efficiency
+        
